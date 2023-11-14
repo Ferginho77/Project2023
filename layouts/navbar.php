de99c530fd431247e4d718b4e6db67eb112ca429
fergie <fergiefakhirusalam@gmail.com>
2023-11-14 21:53:25 +0700

    p

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -5,8 +5,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
 
-    <link rel="stylesheet" href="user.css">
-    <link rel="stylesheet" href="../css/user.css">
+    
+  <link rel="stylesheet" href="css/user.css">
     <title>ADMIN</title>
 </head>
 <nav class="navbar navbar-expand-lg bg-#292832 navbar-dark text-light">
